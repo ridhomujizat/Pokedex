8b0907966aef299cb96385e6b7d772139c4e44e8
ridhomujizat <ridhomujizat@gmail.com>
2021-04-20 05:42:10 +0700

    init projeck pokemondex

---
+++ src/pages/PokemonList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class PokemonList extends Component {
+  render () {
+    return (
+      <div className="container">
+        <p>Pokemon List</p>
+      </div>
+    )
+  }
+}
